c2a56a3f99ef21dce73c6a9a9f39a1b12baac0fa
Ascyt <account@ascyt.com>
2024-10-09 22:52:39 +0200

    Added privacy policy, without hyperlinks yet

--- Documentation/src/app/app.routes.ts
+++ Documentation/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { RedirectGuard } from './redirect.guard';
 import { RedirectingComponent } from './redirecting/redirecting.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -10,6 +11,8 @@ export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'source', component: RedirectingComponent, canActivate: [RedirectGuard], data: {externalUrl: 'https://github.com/Ascyt/www'}},
     {path: 'src', redirectTo: '/source', pathMatch: 'full'},
+
+    {path: 'privacy-policy', component: PrivacyPolicyComponent},
 
     {path: '**', component: NotFoundComponent}
 ];
