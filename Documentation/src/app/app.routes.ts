2c4f20b947174c31997b3c5321929510a243f46d
Ascyt <account@ascyt.com>
2024-10-10 13:35:07 +0200

    Added terms and conditions

--- Documentation/src/app/app.routes.ts
+++ Documentation/src/app/app.routes.ts
@@ -4,15 +4,18 @@ import { RedirectGuard } from './redirect.guard';
 import { RedirectingComponent } from './redirecting/redirecting.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
 
     {path: 'home', component: HomeComponent},
-    {path: 'source', component: RedirectingComponent, canActivate: [RedirectGuard], data: {externalUrl: 'https://github.com/Ascyt/www'}},
+    {path: 'source', component: RedirectingComponent, canActivate: [RedirectGuard], data: {externalUrl: 'https://github.com/Ascyt/open-qotd'}},
+    {path: 'license', component: RedirectingComponent, canActivate: [RedirectGuard], data: {externalUrl: 'https://github.com/Ascyt/open-qotd/blob/main/LICENSE'}},
     {path: 'src', redirectTo: '/source', pathMatch: 'full'},
 
     {path: 'privacy-policy', component: PrivacyPolicyComponent},
+    {path: 'terms-of-service', component:TermsOfServiceComponent},
 
     {path: '**', component: NotFoundComponent}
 ];
