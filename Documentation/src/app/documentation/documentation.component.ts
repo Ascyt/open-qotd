0dc872b5e41e2c6fd6345cdda9e630bec5de22d3
Ascyt <account@ascyt.com>
2024-10-12 21:08:11 +0200

    Finished documentation on Config

--- Documentation/src/app/documentation/documentation.component.ts
+++ Documentation/src/app/documentation/documentation.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { ThemeSwitcherService } from '../theme-switcher/theme-switcher.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-documentation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.scss'
 })
 export class DocumentationComponent {
-
+  constructor(public themeSwitcherService:ThemeSwitcherService) {}
 }
