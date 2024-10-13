47615fc18da6575d0f9191e4fe8ff0171075249a
Ascyt <account@ascyt.com>
2024-10-13 21:22:15 +0200

    Added about

--- Documentation/src/app/about/about.component.ts
+++ Documentation/src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgbModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
