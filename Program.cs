dcf3d7c8d983a5fe9c1c32795a59f1c64e43bbac
Ascyt <account@ascyt.com>
2024-11-24 20:09:12 +0100

    Removed message content intent

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ namespace CustomQotd
                 Environment.Exit(1);
             }
 
-            DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(discordToken, TextCommandProcessor.RequiredIntents | SlashCommandProcessor.RequiredIntents | DiscordIntents.MessageContents);
+            DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(discordToken, SlashCommandProcessor.RequiredIntents);
 
             // Use the commands extension
             builder.UseCommands
