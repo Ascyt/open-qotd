65a5568c2b517b2913a9fd7eaed82610b163eabe
Ascyt <account@ascyt.com>
2024-11-24 20:10:34 +0100

    Readded TextCommandProcessor required intents

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ namespace CustomQotd
                 Environment.Exit(1);
             }
 
-            DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(discordToken, SlashCommandProcessor.RequiredIntents);
+            DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(discordToken, TextCommandProcessor.RequiredIntents | SlashCommandProcessor.RequiredIntents);
 
             // Use the commands extension
             builder.UseCommands
