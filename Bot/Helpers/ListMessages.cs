5e7fa55255943ece60eee307b206a7fa35f781f2
Ascyt <account@ascyt.com>
2025-10-05 13:45:42 +0200

    Fixes and minor improvement to list message

--- Bot/Helpers/ListMessages.cs
+++ Bot/Helpers/ListMessages.cs
@@ -91,6 +91,13 @@ namespace OpenQotd.Bot.Helpers
 
             while (!result.TimedOut && result.Result?.Id != null)
             {
+                if (result.Result.User.Id != context.User.Id)
+                {
+                    await EventHandlers.EventHandlers.RespondWithError(result.Result, "You cannot interact with a different user's list message.");
+                    result = await message.WaitForButtonAsync();
+                    continue;
+                }
+
                 switch (result.Result.Id)
                 {
                     case "first":
@@ -165,10 +172,10 @@ namespace OpenQotd.Bot.Helpers
         private static void AddPaginationButtonsToMessage<T>(IDiscordMessageBuilder messageBuilder, PageInfo<T> pi)
         {
             messageBuilder.AddActionRowComponent(
-                new DiscordButtonComponent(DiscordButtonStyle.Secondary, "first", "<<", disabled: pi.CurrentPage == 1),
-                new DiscordButtonComponent(DiscordButtonStyle.Primary, "backward", "<", disabled: pi.CurrentPage == 1),
-                new DiscordButtonComponent(DiscordButtonStyle.Primary, "forward", ">", disabled: pi.CurrentPage == pi.TotalPagesCount),
-                new DiscordButtonComponent(DiscordButtonStyle.Secondary, "last", ">>", disabled: pi.CurrentPage == pi.TotalPagesCount)
+                new DiscordButtonComponent(DiscordButtonStyle.Secondary, "first", "⭰", disabled: pi.CurrentPage == 1),
+                new DiscordButtonComponent(DiscordButtonStyle.Primary, "backward", "⭠", disabled: pi.CurrentPage == 1),
+                new DiscordButtonComponent(DiscordButtonStyle.Primary, "forward", "⭢", disabled: pi.CurrentPage == pi.TotalPagesCount),
+                new DiscordButtonComponent(DiscordButtonStyle.Secondary, "last", "⭲", disabled: pi.CurrentPage == pi.TotalPagesCount)
             );
         }
 
