25d89b21d57f234d6d1d380960092d234a6503b2
Ascyt <contact@ascyt.com>
2025-10-07 10:43:35 +0200

    Added random periodic statuses from presets

---
+++ Bot/ActivitySwitcher/ActivitySwitcherTimer.cs
@@ -0,0 +1,46 @@
+﻿using DSharpPlus.Entities;
+
+namespace OpenQotd.Bot.ActivitySwitcher
+{
+    /// <summary>
+    /// Periodic timer that updates the bot's activity to a random preset.
+    /// </summary>
+    public class ActivitySwitcherTimer
+    {
+        private static readonly Random _random = new();
+        private static string? _lastActivity = null;
+
+        public static async Task ActivitySwitchLoopAsync(CancellationToken ct)
+        {
+            Console.WriteLine("Started activity switch loop.");
+            while (true)
+            {
+                try
+                {
+                    await SwitchToRandomActivityAsync();
+                    await Task.Delay(Program.AppSettings.ActivitySwitchLoopDelayMs, ct);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error in FetchLoopAsync:\n{ex.Message}");
+                }
+            }
+        }
+
+        public static async Task SwitchToRandomActivityAsync()
+        {
+            int presetIndex = _random.Next(Presets.Values.Length);
+            string activity = Presets.Values[presetIndex];
+
+            if (activity == _lastActivity)
+            {
+                await SwitchToRandomActivityAsync(); // Try again if the same activity was chosen
+                return;
+            }
+            _lastActivity = activity;
+
+            await Program.Client.UpdateStatusAsync(new DiscordActivity(activity, DiscordActivityType.Custom));
+            //await Console.Out.WriteLineAsync($"[{DateTime.UtcNow:O}] Switched activity to: {activity}");
+        }
+    }
+}
