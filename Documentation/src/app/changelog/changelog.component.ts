e527b2611e6656df03ef957869221ec83daae0b3
Ascyt <account@ascyt.com>
2025-10-27 16:53:49 +0100

    Added changelog for v2

--- Documentation/src/app/changelog/changelog.component.ts
+++ Documentation/src/app/changelog/changelog.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ThemeSwitcherService } from '../theme-switcher/theme-switcher.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-changelog',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './changelog.component.html',
   styleUrl: './changelog.component.scss'
 })
 export class ChangelogComponent {
-
+  constructor(public themeSwitcherService:ThemeSwitcherService) {}
 }
