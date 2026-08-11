2318042f3e1be725960ab0edf254dd57e73c9eac
Ascyt <account@ascyt.com>
2026-08-11 19:22:57 +0200

    Added handling for HasModOrIsInSuggestionsChannel when channel is unset

--- Bot/Core/Suggestions/Commands/Helpers/General.cs
+++ Bot/Core/Suggestions/Commands/Helpers/General.cs
@@ -16,6 +16,13 @@ namespace OpenQotd.Core.Suggestions.Commands.Helpers
                 bool isInSuggestionsChannel = config.SuggestionsChannelId is not null && config.SuggestionsChannelId.Value == context.Channel.Id;
                 if (!isInSuggestionsChannel)
                 {
+                    if (config.SuggestionsChannelId is null)
+                    {
+                        await context.RespondAsync(
+                            GenericEmbeds.Error(title: "Suggestions Channel Not Set", message: "The suggestions channel has not been set up yet. Set it up with `/config set suggestions channel` or contact a server administrator."));
+                        return false;
+                    }
+
                     await context.RespondAsync(
                         GenericEmbeds.Error(title: "Incorrect Channel", message: $"This command can only be run in the <#{config.SuggestionsChannelId!.Value}> channel."));
                     return false;
